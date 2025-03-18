348bff0b1c05d52029e9a9fc9c45f9b43b0338d4
LANCECRUZ-STUDENT <g00428876@atu.ie>
2025-03-18 09:49:46 +0000

    Added calculator and test class that tested addition success and failure

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CalculatorTest {
+    Calculator myCalc;
+
+    @BeforeEach
+    void setup()
+    {
+        myCalc = new Calculator();
+    }
+    @Test
+    void testAddSuccess()
+    {
+        assertEquals(8, myCalc.add(4,4));
+    }
+    @Test
+    void testAddFail()
+    {
+        Exception ex = assertThrows(IllegalArgumentException.class, ()-> {myCalc.add(Integer.MAX_VALUE, 4);});
+        assertEquals("The value was too big", ex.getMessage());
+    }
+
+}
